1bf517f57c7391494bd39e69fc9187373fdb951f
Nathalia <nathaliasimmer@hotmail.com>
2016-05-16 03:04:56 -0300

    home

--- app/src/main/java/br/uvv/carona/service/CommomService.java
+++ app/src/main/java/br/uvv/carona/service/CommomService.java
@@ -4,16 +4,20 @@ import br.uvv.carona.application.AppPartiUVV;
 import br.uvv.carona.httprequest.BaseHttpRequest;
 import br.uvv.carona.httprequest.util.HttpMethodUtil;
 import br.uvv.carona.httprequest.util.WSResources;
+import br.uvv.carona.model.Statistic;
 import br.uvv.carona.model.Student;
 import br.uvv.carona.model.UploadFile;
 
 /**
  * Created by Nathalia on 15/05/2016.
  */
-public class UploadService {
+public class CommomService {
     public static UploadFile upload(Student student) throws Exception {
-//        return AppPartiUVV.sGson.fromJson(BaseHttpRequest.createRequest(HttpMethodUtil.POST, WSResources.URL_UPLOAD, student), UploadFile.class);
-        return null;
+        return AppPartiUVV.sGson.fromJson(BaseHttpRequest.createRequest(HttpMethodUtil.POST, WSResources.URL_UPLOAD, student), UploadFile.class);
+    }
+
+    public static Statistic statistics() throws Exception {
+        return AppPartiUVV.sGson.fromJson(BaseHttpRequest.createRequest(HttpMethodUtil.GET, WSResources.URL_STATISTIC, null), Statistic.class);
     }
 
 }
